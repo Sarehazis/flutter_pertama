a0579c77c43d417604ffd8fb762a7056ff1df2d3
Sarehazis <sarehazisp@gmail.com>
2024-10-15 12:50:32 +0700

    commit tipe data

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,63 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_pertama/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  // Type Data & Variable
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+// Variable
+  var mahasiswa = 'Mahasiswa 1';
+  var umur = 20;
+  print(mahasiswa + " Umur : " + umur.toString());
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+// String
+  String mahasiswaString = 'Mahasiswa 2';
+  print(mahasiswaString);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+// Integer
+  int semester = 3;
+  print(semester);
+
+// Double
+  // double ipk = 3.5;
+  // print(ipk);
+  double ipk;
+  ipk = 3.5;
+  print(ipk);
+
+// Boolean
+
+  // bool lakiLaki = false;
+  // print(lakiLaki);
+
+  bool benar = true;
+  bool salah = false;
+  bool tidakbenar = !true;
+  bool tidaksalah = !false;
+
+// List
+
+  // List jurusan = [
+  //   'Teknologi Informasi',
+  //   'Mekatronika',
+  //   'Bahasa Jepang',
+  //   'Bisnis Digital'
+  // ];
+
+  // print(jurusan);
+
+  List<String> jurusan = [
+    'Teknologi Informasi',
+    'Mekatronika',
+    'Bahasa Jepang',
+    'Bisnis Digital',
+    semester.toString(),
+    ipk.toString(),
+  ];
+  print(jurusan);
+
+// Map
+
+  Map mahasiswaMap = {"nama": "Sareh Azis Panegar", "Semester": 3};
+  print(mahasiswaMap);
+  print(mahasiswaMap['nama']);
+  print(mahasiswaMap['Semester']);
 }
